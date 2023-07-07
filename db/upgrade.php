396fba11df4dd917798ad3aaf40bd6a3db49a46b
Patrick Thibaudeau <thibaud@yorku.ca>
2023-07-07 08:59:08 -0400

    Reverted code to previous version

--- db/upgrade.php
+++ db/upgrade.php
@@ -38,5 +38,10 @@ function xmldb_format_menutab_upgrade($oldversion) {
 
     $dbman = $DB->get_manager();
 
+    // For further information please read {@link https://docs.moodle.org/dev/Upgrade_API}.
+    //
+    // You will also have to create the db/install.xml file by using the XMLDB Editor.
+    // Documentation for the XMLDB Editor can be found at {@link https://docs.moodle.org/dev/XMLDB_editor}.
+
     return true;
 }
